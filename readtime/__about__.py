7a33721405863ea3b557048b9fd3225fe432dc1e
Leo Gallucci <elgalu3@gmail.com>
2023-09-01 22:04:57 +0200

    Typo forumula -> formula

--- readtime/__about__.py
+++ readtime/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'readtime'
 __description__ = 'Calculates the time some text takes the average human to ' \
-                  'read, based on Medium\'s read time forumula'
+                  'read, based on Medium\'s read time formula'
 __url__ = 'https://github.com/alanhamlett/readtime'
 __version_info__ = ('3', '0', '0')
 __version__ = '.'.join(__version_info__)
